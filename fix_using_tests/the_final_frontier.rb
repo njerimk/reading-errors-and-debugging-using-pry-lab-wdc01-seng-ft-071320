ae6ec5b88d60068efc8df1c6ff12888f2f962347
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-01 20:15:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,7 +12,8 @@ end
 
 def engage (star_date) 
   state_log="Captain's Log, star date#{star_date}"
-  return state_log + crew_greetings
+  generate_star_date=(rand(100000) + 400000) / 10.0
+  return state_log + generate_star_date
   
 end
 
